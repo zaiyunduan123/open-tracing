696cb861b36b65178f8385509ead3d8e781c32b2
zaiyunduan123 <1778667492@qq.com>
2018-07-21 15:26:59 +0800

    first commit

---
+++ zipkin/src/main/java/com/jesper/sleuth/ZipkinServerApp.java
@@ -0,0 +1,29 @@
+package com.jesper.sleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import zipkin.server.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinServerApp {
+
+    /**
+     * zipkin和mysql结合保存zipkin在项目监控中得到的所有数据
+     * @param datasource
+     * @return
+     */
+    @Bean
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApp.class, args);
+    }
+
+}
